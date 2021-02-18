1a61480414dd45c1040d64ec385402c74a3f07b1
Xiang Chen <xiangchenchen96@gmail.com>
2021-02-18 02:12:27 +0100

    :zap: Improve force graph

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,23 +5,28 @@ import * as d3 from "d3";
 import waifus from "./waifus.json";
 import waifu_links from "./waifu_links.json";
 
+const calculateAvg = (arr) => arr.reduce((a, b) => a + b) / arr.length;
+const data = {
+  nodes: Object.values(waifus).map((node) => {
+    const img = new Image();
+    img.src = node.display_picture;
+
+    return {
+      id: node.id,
+      text: node.name,
+      img,
+    };
+  }),
+  links: waifu_links,
+};
+
 function App() {
   const fgRef = useRef();
-  const data = {
-    nodes: Object.values(waifus).map((node) => {
-      const img = new Image();
-      img.src = node.display_picture;
+  const size = 30;
+  const avgLinkValue = calculateAvg(waifu_links.map((a) => a.value));
 
-      return {
-        id: node.id,
-        text: node.name,
-        img,
-      };
-    }),
-    links: waifu_links,
-  };
   useEffect(() => {
-    fgRef.current.d3Force("collide", d3.forceCollide().radius(10));
+    fgRef.current.d3Force("collide", d3.forceCollide().radius(15));
     fgRef.current.d3Force("link").distance((link) => 300_000 * link.value);
   }, []);
 
@@ -30,10 +35,15 @@ function App() {
       <ForceGraph2D
         ref={fgRef}
         graphData={data}
+        nodeRelSize={size}
         nodeCanvasObject={({ img, x, y }, ctx) => {
-          const size = 30;
-          ctx.drawImage(img, x - size / 2, y - size / 2, size, size);
+          ctx.fillStyle = "#FFFFFF";
+          x = x - size / 2;
+          y = y - size / 2;
+          ctx.fillRect(x, y, size, size);
+          ctx.drawImage(img, x, y, size, size);
         }}
+        linkVisibility={(link) => link.value < avgLinkValue}
       />
     </div>
   );
